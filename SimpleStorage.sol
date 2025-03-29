548ce4c422e9b720eb407bc5f69ef2f9519eb38f
FourSqweez <jeerasak.code@gmail.com>
2025-03-29 12:32:05 +0700

    Function

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,7 +4,18 @@ pragma solidity 0.8.18;
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value is given
-    uint256 favoriteNumber; // 0
+    uint256 public favoriteNumber; // 0
+
+
+    function store(uint256 _favoriteNumber) public  {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure function no need to spend gas, no need to send transaction
+    function retrive() public  view returns(uint256) {
+        return favoriteNumber;
+    }
+
 
 
 }
